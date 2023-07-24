4c9e8046b9dc78c194a6d8be5d43ac5a1e6146b1
zhupailiangx <120553507+zhupailiangx@users.noreply.github.com>
2023-07-24 11:02:48 +0800

    Update build_perf.bat

--- OpenCV_Perf/build_perf.bat
+++ OpenCV_Perf/build_perf.bat
@@ -8,5 +8,5 @@ cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_PERF_TESTS=ON ..
 # opencv_perfcalib3d,opencv_perf_dnn,opencv_perf_features2d,opencv_perf_imgcodes,opencv_perf_objdetect,opencv_perf_photo,opencv_perf_stitching,opencv_perf_video,opencv_perf_videoio,opencv_perf_core,opencv_perf_imgproc
 cmake --build . --target opencv_perf_core opencv_perf_imgproc  --config Release
 cd bin/Release
-./opencv_perf_core.exe
-./opencv_perf_imgproc.exe
+opencv_perf_core.exe
+opencv_perf_imgproc.exe
