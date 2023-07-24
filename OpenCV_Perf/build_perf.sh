626c82dec2a59d7ff76735523df44e71c25c970e
zhupailiangx <120553507+zhupailiangx@users.noreply.github.com>
2023-07-24 12:53:28 +0800

    Update build_perf.sh

--- OpenCV_Perf/build_perf.sh
+++ OpenCV_Perf/build_perf.sh
@@ -8,7 +8,3 @@ cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_PERF_TESTS=ON ..
 # opencv_perf_calib3d,opencv_perf_dnn,opencv_perf_features2d,opencv_perf_imgcodes,opencv_perf_objdetect,opencv_perf_photo,opencv_perf_stitching,opencv_perf_video,opencv_perf_videoio,opencv_perf_core,opencv_perf_imgproc
 make -j$(nproc) opencv_perf_core opencv_perf_imgproc
-cd bin
-./opencv_perf_core
-./opencv_perf_imgproc
-
