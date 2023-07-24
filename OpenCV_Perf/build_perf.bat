eb1bc4ee360fb65e3108d428bf72e089ccfb0313
zhupailiangx <120553507+zhupailiangx@users.noreply.github.com>
2023-07-24 10:08:44 +0800

    Update build_perf.bat

--- OpenCV_Perf/build_perf.bat
+++ OpenCV_Perf/build_perf.bat
@@ -4,3 +4,6 @@ if not exist build\ (
 cd build
 cmake -DBUILD_PERF_TESTS=ON ..
 cmake --build . --target opencv_perf_core opencv_perf_imgproc  --config Release
+cd bin/Release
+./opencv_perf_core.exe
+./opencv_perf_imgproc.exe
