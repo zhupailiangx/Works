36c12060f72f4d1c06bc621afd1ed4278ad0ea6e
zhupailiangx <120553507+zhupailiangx@users.noreply.github.com>
2023-10-07 15:53:53 +0800

    Add files via upload

---
+++ Opencv_tests_selected/opencv_test_results_to_excel.py
@@ -0,0 +1,41 @@
+import argparse
+import pandas as pd
+
+def main(args):
+    df = pd.read_html(args.input_path)
+    df = pd.DataFrame(df[0])
+    df['Skew']=args.Skew
+    df['File Name'] = args.File_Name
+    df['Memory type'] = args.Memory_type
+    df['OS'] = args.OS
+    df['OpenCV version'] = args.OpenCV_version
+    df['OpenCV install'] = args.OpenCV_install
+    df['OpenCL'] = args.OpenCL
+    df['Memory Speed'] = args.Memory_Speed
+    df['WW'] = args.WW
+    df['CPU_req'] = args.CPU_req
+    #df = df.sort_values(by='Name of Test',ascending=True)
+    df.to_excel(args.output_path,index=False)
+    print(df)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='opencv_tests')
+    parser.add_argument('--input_path',default=r"C:\Users\pailianx\Downloads\opencv-12700-10-07\imgproc_selected.ods", type=str,help ='input ods file')
+    parser.add_argument('--output_path',default = r"C:\Users\pailianx\Downloads\opencv-12700-10-07\imgproc_selected2.xlsx", type=str,help='result excel file')
+    parser.add_argument('--Skew', default="12700", type=str, help='Machine type')
+    parser.add_argument('--File_Name',default="core" ,type=str, action='store',help='Moudle')
+    parser.add_argument('--Memory_type', default="DDR4", type=str, help='Memory type')
+    parser.add_argument('--OS', default="Ubuntu:22.04", type=str, help='OS version')
+    parser.add_argument('--OpenCV_version', default="v4.8.0", type=str, help='OpenCV version')
+    parser.add_argument('--OpenCV_install', default="Install", type=str, help='OpenCV install')
+    parser.add_argument('--OpenCL', default="disabled", type=str, help='OpenCL')
+    parser.add_argument('--Memory_Speed', default="3200MT/s", type=str, help='Memory Speed')
+    parser.add_argument('--WW', default="WW40", type=str, help='WW')
+    parser.add_argument('--CPU_req', default="2100MHz", type=str, help='CPU_req')
+
+
+
+
+    args = parser.parse_args()
+    main(args)
---
+++ Opencv_tests_selected/opencv_test_selected.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ ! -d build ]; then
+	mkdir build
+fi
+cd build
+cmake -DWITH_TBB=OFF -DWITH_OPENMP=OFF -DWITH_PTHREADS_PF=OFF ..
+make opencv_perf_core opencv_perf_imgproc -j8
+
+
+cd bin
+
+export OPENCV_OPENCL_DEVICE=disabled
+
+python3 ../../modules/ts/misc/run.py -t core --gtest_filter=Size_MatType_inRange.inRange/16:Size_MatType_bitwise_and.bitwise_and/10:Size_MatType_NormType_normalize.normalize/20:Size_Depth_Channels_split.split/26:Size_MatType_NormType_norm.norm/20:Size_MatType_bitwise_not.bitwise_not/10:BinaryOpTest.subtract/20 --gtest_output=xml:core_selected.xml --perf_force_samples=1000 --perf_min_samples=1000
+
+python3 ../../modules/ts/misc/run.py -t imgproc --gtest_filter=Size_Source_calcHist1d.calcHist1d/0:Size_CvtMode_cvtColor8u.cvtColor8u/243:Size_CvtMode_cvtColor8u.cvtColor8u/358:TestFindContours_findContours.findContours/49:TestFindContours_findContours.findContours/33:Size_MatType_ThreshType_threshold.threshold/50:Size_MatType_ThreshType_threshold.threshold/40:Size_MatType_BorderType3x3_gaussianBlur3x3.gaussianBlur3x3/39:Size_MatType_BorderType_gaussianBlur5x5.gaussianBlur5x5/77:TestWarpAffine_WarpAffine.WarpAffine/10:TestWarpAffine_WarpAffine.WarpAffine/9:Size_MatType_dilate.dilate/8:TestBoundingRect_BoundingRect.BoundingRect/9:Size_MatType_erode.erode/8:OCL_EqualizeHistFixture_EqualizeHist.EqualizeHist/2:Size_MatType_dx_dy_Border3x3_sobelFilter.sobelFilter/63:Size_MatType_dx_dy_Border5x5_sobelFilter.sobelFilter/105:Size_MatType_BorderType3x3_blur3x3.blur3x3/31:Size_MatType_BorderType_blur5x5.blur5x5/25:Size_MatType_kSize_medianBlur.medianBlur/30:Size_MatType_kSize_medianBlur.medianBlur/31:Transform.getPerspectiveTransform_1000:TestWarpPerspectiveNear_t_WarpPerspectiveNear.WarpPerspectiveNear/12:Sz_ClipLimit_CLAHE.CLAHE/8:TestFilter2d_Filter2d.Filter2d/6:MomentsFixture_val_Moments1.Moments1/16:TestBilateralFilter_BilateralFilter.BilateralFilter/14 --gtest_output=xml:imgproc_selected.xml --perf_force_samples=1000 --perf_min_samples=1000
+
+python3 ../../modules/ts/misc/report.py core_selected.xml -o html >core_selected.ods
+
+python3 ../../modules/ts/misc/report.py imgproc_selected.xml -o html >imgproc_selected.ods
+
+
