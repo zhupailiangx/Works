0c325dfcd7e4e47514a9c52c0dde697629f122ad
zhupailiangx <120553507+zhupailiangx@users.noreply.github.com>
2023-10-08 10:53:19 +0800

    Update opencv_test_results_to_excel.py

--- Opencv_tests_selected/opencv_test_results_to_excel.py
+++ Opencv_tests_selected/opencv_test_results_to_excel.py
@@ -2,29 +2,70 @@ import argparse
 import pandas as pd
 
 def main(args):
-    df = pd.read_html(args.input_path)
-    df = pd.DataFrame(df[0])
-    df['Skew']=args.Skew
-    df['File Name'] = args.File_Name
-    df['Memory type'] = args.Memory_type
-    df['OS'] = args.OS
-    df['OpenCV version'] = args.OpenCV_version
-    df['OpenCV install'] = args.OpenCV_install
-    df['OpenCL'] = args.OpenCL
-    df['Memory Speed'] = args.Memory_Speed
-    df['WW'] = args.WW
-    df['CPU_req'] = args.CPU_req
-    #df = df.sort_values(by='Name of Test',ascending=True)
-    df.to_excel(args.output_path,index=False)
-    print(df)
+    #print(args.File_Name.split(','))
+    path_list= args.input_path.split(',')
+    #print(path_list[0])
+    if len(path_list)==1:
+        df = pd.read_html(path_list[0])
+        df = pd.DataFrame(df[0])
+
+        df['Skew']=args.Skew
+        df['File Name'] = args.File_Name
+
+        df['Memory type'] = args.Memory_type
+        df['OS'] = args.OS
+        df['OpenCV version'] = args.OpenCV_version
+        df['OpenCV install'] = args.OpenCV_install
+        df['OpenCL'] = args.OpenCL
+        df['Memory Speed'] = args.Memory_Speed
+        df['WW'] = args.WW
+        df['CPU_req'] = args.CPU_req
+        #df = df.sort_values(by='Name of Test',ascending=True)
+        print(df)
+        df.to_excel(args.output_path,index=False)
+
+    else:
+        df = pd.read_html(path_list[0])
+        df = pd.DataFrame(df[0])
+
+        df['Skew'] = args.Skew
+        df['File Name'] = args.File_Name.split(',')[0]
+
+        df['Memory type'] = args.Memory_type
+        df['OS'] = args.OS
+        df['OpenCV version'] = args.OpenCV_version
+        df['OpenCV install'] = args.OpenCV_install
+        df['OpenCL'] = args.OpenCL
+        df['Memory Speed'] = args.Memory_Speed
+        df['WW'] = args.WW
+        df['CPU_req'] = args.CPU_req
+
+        df_2 = pd.read_html(path_list[1])
+        df_2 = pd.DataFrame(df_2[0])
+        #print(df_2)
+        df_2['Skew'] = args.Skew
+        df_2['File Name'] = args.File_Name.split(',')[1]
+        #print(args.File_Name)
+        df_2['Memory type'] = args.Memory_type
+        df_2['OS'] = args.OS
+        df_2['OpenCV version'] = args.OpenCV_version
+        df_2['OpenCV install'] = args.OpenCV_install
+        df_2['OpenCL'] = args.OpenCL
+        df_2['Memory Speed'] = args.Memory_Speed
+        df_2['WW'] = args.WW
+        df_2['CPU_req'] = args.CPU_req
+
+        df_final= pd.concat([df,df_2])
+        df_final.to_excel(args.output_path, index=False)
+
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='opencv_tests')
-    parser.add_argument('--input_path',default=r"C:\Users\pailianx\Downloads\opencv-12700-10-07\imgproc_selected.ods", type=str,help ='input ods file')
-    parser.add_argument('--output_path',default = r"C:\Users\pailianx\Downloads\opencv-12700-10-07\imgproc_selected2.xlsx", type=str,help='result excel file')
+    parser.add_argument('--input_path',default=r"C:\Users\pailianx\Downloads\opencv-12700-10-07\core_imgproc_selected.ods", type=str,help ='input ods files')
+    parser.add_argument('--output_path',default = r"C:\Users\pailianx\Downloads\opencv-12700-10-07\core_imgproc_selected2.xlsx", type=str,help='result excel file')
     parser.add_argument('--Skew', default="12700", type=str, help='Machine type')
-    parser.add_argument('--File_Name',default="core" ,type=str, action='store',help='Moudle')
+    parser.add_argument('--File_Name',default="core",type=str, action='store',help='Moudle')
     parser.add_argument('--Memory_type', default="DDR4", type=str, help='Memory type')
     parser.add_argument('--OS', default="Ubuntu:22.04", type=str, help='OS version')
     parser.add_argument('--OpenCV_version', default="v4.8.0", type=str, help='OpenCV version')
@@ -33,9 +74,6 @@ if __name__ == "__main__":
     parser.add_argument('--Memory_Speed', default="3200MT/s", type=str, help='Memory Speed')
     parser.add_argument('--WW', default="WW40", type=str, help='Weeks')
     parser.add_argument('--CPU_req', default="2100MHz", type=str, help='CPU_req')
-
-
-
 
     args = parser.parse_args()
     main(args)
