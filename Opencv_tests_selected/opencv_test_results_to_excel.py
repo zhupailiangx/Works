1a6de0a6630bf85b20ccbcbf72e992b61a59e9c8
zhupailiangx <120553507+zhupailiangx@users.noreply.github.com>
2023-11-02 13:23:52 +0800

    Update opencv_test_results_to_excel.py

--- Opencv_tests_selected/opencv_test_results_to_excel.py
+++ Opencv_tests_selected/opencv_test_results_to_excel.py
@@ -68,7 +68,31 @@ def main(args):
         df_2['WW'] = args.WW
         df_2['CPU_req'] = args.CPU_req
 
-        df_final= pd.concat([df,df_2])
+        
+        try:
+            df_3 = pd.read_html(path_list[2])
+        except:
+            print("Error 4: File not found or failed to read file")
+
+        df_3 = pd.DataFrame(df_3[0])
+        #print(df_2)
+        df_3['Skew'] = args.Skew
+        df_3['File Name'] = args.File_Name.split(',')[2]
+        #print(args.File_Name)
+        df_3['Memory type'] = args.Memory_type
+        df_3['OS'] = args.OS
+        df_3['OpenCV version'] = args.OpenCV_version
+        df_3['OpenCV install'] = args.OpenCV_install
+        df_3['OpenCL'] = args.OpenCL
+        df_3['Memory Speed'] = args.Memory_Speed
+        df_3['WW'] = args.WW
+        df_3['CPU_req'] = args.CPU_req
+         
+        df_sort = pd.read_excel(args.opencv_lists)
+         
+        df_final= pd.concat([df,df_2,df_3])
+        df_final = pd.merge(df_sort,df_final,on="Name of Test",how='left')
+        
         df_final.to_excel(args.output_path, index=False)
 
         print("Run successfully")
@@ -78,7 +102,8 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='opencv_tests')
     parser.add_argument('--input_path',default=r"core_imgproc_selecte.ods", type=str,help ='input ods files')
-    parser.add_argument('--output_path',default = r"core_imgproc_selected2.xlsx", type=str,help='result excel file')
+    parser.add_argument('--output_path',default = "../../opencv result data.xlsx", type=str,help='result excel file')
+    parser.add_argument('--opencv_lists',default = "../../opencv_test_lists.xlsx", type=str,help='result excel file')
     parser.add_argument('--Skew', default="12700", type=str, help='Machine type')
     parser.add_argument('--File_Name',default="core",type=str, action='store',help='Moudle')
     parser.add_argument('--Memory_type', default="DDR4", type=str, help='Memory type')
@@ -88,10 +113,11 @@ if __name__ == "__main__":
     parser.add_argument('--OpenCL', default="disabled", type=str, help='OpenCL')
     parser.add_argument('--Memory_Speed', default="3200MT/s", type=str, help='Memory Speed')
     parser.add_argument('--WW', default="WW40", type=str, help='Weeks')
-    parser.add_argument('--CPU_req', default="2100MHz", type=str, help='CPU base frequency')
+    parser.add_argument('--CPU_req', default=" ", type=str, help='CPU base frequency')
 
 
 
 
     args = parser.parse_args()
     main(args)
+
